f9534afac7cd791429507956f2dcd9bea85deaa5
Miraj Asraf <75885581+Miraj8280@users.noreply.github.com>
2023-01-19 00:59:33 +0530

    Update Lecture22.java

--- Lecture22.java
+++ Lecture22.java
@@ -1,4 +1,4 @@
-// Encapsulation => Encapsulation is a mechanism throgh which we can wrapping the data members and member methods of a class in a single unit.
+// Encapsulation => Encapsulation is a mechanism through which we can wrapping the data members and member methods of a class in a single unit.
 /*
     i) Declare the class variables as private. => data hiding
     ii) Declare the class methods as public. => data abstruction
